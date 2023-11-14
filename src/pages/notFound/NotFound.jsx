455e85d4b1f6ad185929fc493fd16914d0c6f252
Taxontes <ismaelvt96@gmail.com>
2023-11-14 16:42:10 +0100

    agregar apartado notFound

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import './notFound.css' 
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <>
+      <div className='contenedor-notFound'>
+        <div className="informacion-notFound">
+          <h1>Not Found</h1>
+          <h3>La url introducida no existe</h3>
+          <img className='img-notFound' src="/snorNotFound.png" alt="Snorlax notFound" />
+          <a className="btn-notfound" href='/'>Volver a inicio</a>
+        </div>
+      </div>
+    
+    </>
+    
   )
 }
 export default NotFound
